e42540ef5d977d1dfb2b67d552c1c4724d3396f3
Maciek Dubiński <maciek@dubinski.net>
2019-09-03 20:36:17 +0200

    Clean up logger comment

--- lib/rss_observer/logger.rb
+++ lib/rss_observer/logger.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module RssObserver
-  # Basic handler, outputting the memory change information to Rails.logger
+  # Basic handler, outputting the memory change information to standard output
   class Logger
     # @param logger [Logger]
     def initialize(logger = Logger.new(STDOUT))
